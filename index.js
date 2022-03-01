cd44dc7caabc2532d3f9c597779344dc023d8119
yjaju <yjaju@usc.edu>
2022-03-01 15:10:22 -0800

    change slack bot to slack app

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const SlackBot = require('slackbots');
-const axios = require('axios')
-const dotenv = require('dotenv')
+const { App } = require('@slack/bolt');
+require('dotenv').config();
 
-dotenv.config()
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000
+});
 
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
 
-const bot = new SlackBot({
-    token: `${process.env.BOT_TOKEN}`,
-    name: 'dev_bot'
-})
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log('⚡️ Bolt app is running!');
+})();
